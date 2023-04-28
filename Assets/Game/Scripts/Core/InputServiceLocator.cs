e3d4bf84fccfe4f877433685354738d6738000c9
philip.Igo <philip.igo@xrgames.io>
2023-04-28 10:24:14 +0100

    Added a way to get the player input generated class.

---
+++ Assets/Game/Scripts/Core/InputServiceLocator.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Game.Scripts.Core
+{
+    public class InputServiceLocator : MonoBehaviour
+    {
+        private static InputManager playerInput;
+
+        public static InputManager GetPlayerInput()
+        {
+            return playerInput ??= new InputManager();
+        }
+    }
+}
