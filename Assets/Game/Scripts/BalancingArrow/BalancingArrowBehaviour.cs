0e50802be3f4e126d8dcad68fcaa105e496ff202
AidanRMurray24 <aidanrmurray24@gmail.com>
2023-04-28 16:27:26 +0100

    Added the balancing script, prefabs and scene

---
+++ Assets/Game/Scripts/BalancingArrow/BalancingArrowBehaviour.cs
@@ -0,0 +1,142 @@
+using System;
+using Game.Scripts.Core;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BalancingArrowBehaviour : MonoBehaviour
+{
+	[SerializeField, Range(0, 360)]
+	private float angleLimit = 45f;
+
+	[SerializeField, Range(0, 360)]
+	private float dangerZoneAngle = 35f;
+
+	[SerializeField]
+	private float inputStrength = 0.5f;
+
+	[SerializeField]
+	private float defaultUnbalanceAmount = 0.3f;
+
+	[SerializeField]
+	private float gravityStrength = 0.1f;
+
+	[SerializeField]
+	private float noiseBias = 0.04f;
+
+	private InputManager inputManager;
+	private float balanceInput = 0.0f;
+	private float currentRotation = 0.0f;
+	private bool hasLostBalance = false;
+	private bool inDangerZone = false;
+	private float previousRotation = 0.0f;
+
+	private event Action OnBalanceLost;
+	private event Action OnDangerZoneEnter;
+	private event Action OnDangerZoneExit;
+
+	private void Awake() => inputManager = InputServiceLocator.GetPlayerInput();
+
+	private void OnEnable()
+	{
+		inputManager.Enable();
+		inputManager.Gameplay.Balance.performed += UpdateBalanceInput;
+		inputManager.Gameplay.Balance.canceled += ResetBalanceInput;
+	}
+
+	private void Update()
+	{
+		if (hasLostBalance) return;
+
+		currentRotation += balanceInput;
+
+		var randomForce = ((Mathf.PerlinNoise1D(Time.time) + noiseBias) * 2 - 1f) * defaultUnbalanceAmount;
+		Debug.Log($"Random Force: {randomForce}");
+		ApplyRotationForce(randomForce);
+		ApplyGravity();
+	}
+
+	private void LateUpdate()
+	{
+		if (hasLostBalance) return;
+
+		// Smoothly move to the target rotation
+		var lerpedRotation = Mathf.Lerp(previousRotation, currentRotation, 0.5f);
+		previousRotation = currentRotation;
+		var newRotation = Quaternion.Euler(0, 0, lerpedRotation);
+		transform.rotation = newRotation;
+
+		var rotation = transform.rotation.eulerAngles.z;
+
+		// Check if we're in the danger zone
+		if (rotation >= dangerZoneAngle && rotation <= 360f - dangerZoneAngle)
+		{
+			EnterDangerZone();
+		}
+		else
+		{
+			ExitDangerZone();
+		}
+
+		// Check if the rotation has exceeded the limit
+		if (rotation >= angleLimit && rotation <= 360f - angleLimit)
+		{
+			BalanceLost();
+		}
+	}
+
+	private void OnDisable()
+	{
+		inputManager.Gameplay.Balance.performed -= UpdateBalanceInput;
+		inputManager.Gameplay.Balance.canceled -= ResetBalanceInput;
+		inputManager.Disable();
+	}
+
+	private void UpdateBalanceInput(InputAction.CallbackContext obj) => balanceInput = -obj.ReadValue<float>() * inputStrength;
+
+	private void ResetBalanceInput(InputAction.CallbackContext obj) => balanceInput = 0f;
+
+	private void ApplyRotationForce(float force) => currentRotation += force;
+
+	private void ApplyGravity()
+	{
+		float gravityAffectiveness = 1.0f - Vector2.Dot(transform.up, Vector2.up);
+
+		var balanceDirection = currentRotation / Mathf.Abs(currentRotation);
+		ApplyRotationForce(gravityAffectiveness * gravityStrength * balanceDirection);
+	}
+
+	private void BalanceLost()
+	{
+		hasLostBalance = true;
+		Debug.Log("Balance Lost!");
+		OnBalanceLost?.Invoke();
+	}
+
+	private void EnterDangerZone()
+	{
+		// Return if we've already entered the danger zone
+		if (inDangerZone) return;
+		inDangerZone = true;
+
+		Debug.Log("Entered DangerZone!");
+		OnDangerZoneEnter?.Invoke();
+	}
+
+	private void ExitDangerZone()
+	{
+		// If we weren't in the DangerZone, return
+		if (!inDangerZone) return;
+		inDangerZone = false;
+		Debug.Log("Exited DangerZone!");
+		OnDangerZoneExit?.Invoke();
+	}
+
+	[Button]
+	private void ResetArrow()
+	{
+		hasLostBalance = false;
+		inDangerZone = false;
+		currentRotation = 0.0f;
+	}
+}
