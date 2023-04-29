9b146d14af432f033579d67424dd83ff9cacdb5f
Gabriele Zotti <gabriele.zotti@xrgames.io>
2023-04-29 15:06:20 +0100

    Added throwing functionality to juggling behaviour

--- Assets/Game/Scripts/Gameplay/JuggleBall.cs
+++ Assets/Game/Scripts/Gameplay/JuggleBall.cs
@@ -1,49 +1,109 @@
-﻿using Sirenix.OdinInspector;
+﻿using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace JugglingRaccoons.Gameplay
 {
 	public class JuggleBall : MonoBehaviour
 	{
 		[SerializeField]
-		private float juggleHeight = 3;
-		[SerializeField]
-		private float juggleTime = 1;
-		[SerializeField]
-		private float handPassHeight = 0.1f;
-		[SerializeField]
-		private float handPassTime = 0.3f;
-		[SerializeField]
 		private float rotateSpeed = 123f;
-		
-		private float totalJuggleTime => juggleTime + handPassTime;
 
-		private float time;
-		private bool isPassing;
+		public JuggleState State { get; private set; }
+		public float Delay { get; set; }
 		
-		public void SetInitialJugglePosition(float t)
+		private JugglingBehaviour jugglingBehaviour;
+		private Transform startTransform;
+		private Transform endTransform;
+		private float travelTime;
+
+		public event Action<JuggleBall> OnNewJuggle;
+		public event Action<JuggleBall> OnReachedTarget;
+
+		private void Awake()
 		{
-			time = juggleTime * t;
 			transform.Rotate(0,0, Random.Range(0, 360));
-			Debug.Log($"Setting t to {t}");
 		}
 
-		public void Move(Vector3 start, Vector3 end)
+		public void SetJugglingBehaviour(JugglingBehaviour jugglingBehaviour)
 		{
-			time += Time.deltaTime;
-			while (time > totalJuggleTime)
+			this.jugglingBehaviour = jugglingBehaviour;
+			startTransform = this.jugglingBehaviour.StartingHand;
+			endTransform = this.jugglingBehaviour.PassingHand;
+		}
+
+		public void Throw(JugglingBehaviour target)
+		{
+			State = JuggleState.Throwing;
+			endTransform = target.StartingHand;
+		}
+		
+		private void Update()
+		{
+			if(!jugglingBehaviour) return;
+
+			if (Delay > 0)
 			{
-				time -= totalJuggleTime;
+				// Keep following the hand
+				transform.position = startTransform.position;
+				Delay -= Time.deltaTime;
+				return;
+			}
+			
+			travelTime += Time.deltaTime;
+			transform.Rotate(0,0, rotateSpeed * Time.deltaTime);
+
+			if (State == JuggleState.Throwing)
+			{
+				var throwTime = jugglingBehaviour.ThrowTime;
+				var throwHeight = jugglingBehaviour.ThrowHeight;
+
+				if (travelTime > throwTime)
+				{
+					travelTime -= throwTime;
+					SetJugglingBehaviour(jugglingBehaviour);
+					State = JuggleState.Juggling;
+					OnReachedTarget?.Invoke(this);
+					return;
+				}
+
+				var throwT = travelTime / throwTime;
+				transform.position = JugglingBehaviour.GetParabolicPoint(startTransform.position, endTransform.position, throwHeight, throwT);
+				return;
+			}
+			
+			var totalJuggleTime = jugglingBehaviour.TotalJuggleTime;
+			var juggleTime = jugglingBehaviour.JuggleTime;
+			var handPassTime = jugglingBehaviour.HandPassTime;
+			var juggleHeight = jugglingBehaviour.JuggleHeight;
+			var handPassHeight = jugglingBehaviour.HandPassHeight;
+			
+			while (travelTime > jugglingBehaviour.TotalJuggleTime)
+			{
+				// New juggle
+				travelTime -= totalJuggleTime;
+				OnNewJuggle?.Invoke(this);
 			}
 
-			var juggleT = time / juggleTime;
-			var passT = (time - juggleTime) / handPassTime;
-			isPassing = passT > 0;
+			var juggleT = travelTime / juggleTime;
+			var passT = (travelTime - juggleTime) / handPassTime;
+			var isPassing = passT > 0;
 			var height = (isPassing) ? handPassHeight : juggleHeight;
 			var t = (isPassing) ? 1 - passT : juggleT;
-
-			transform.position = JugglingManager.GetParabolicPoint(start, end, height, t);
-			transform.Rotate(0,0, rotateSpeed * Time.deltaTime);
+			if (isPassing)
+			{
+				State = JuggleState.Passing;
+			}
+			
+			transform.position = JugglingBehaviour.GetParabolicPoint(startTransform.position, endTransform.position, height, t);
 		}
+	}
+
+	public enum JuggleState
+	{
+		Juggling,
+		Passing,
+		Throwing,
+		Falling
 	}
 }
---
+++ Assets/Game/Scripts/Gameplay/JugglingBehaviour.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace JugglingRaccoons.Gameplay
+{
+	public class JugglingBehaviour : MonoBehaviour
+	{
+		[Header("References")]
+		[SerializeField]
+		private GameObject ballPrefab;
+
+		[field: SerializeField]
+		public Transform StartingHand { get; private set; }
+		[field: SerializeField]
+		public Transform PassingHand { get; private set; }
+
+		[Header("Config")]
+		[SerializeField]
+		private int startingBallsCount = 3;
+		[SerializeField]
+		private int maxBallsCount = 10;
+
+		[field: Header("Balls Config")]
+		[field: SerializeField]
+		public float JuggleHeight { get; private set; } = 2.2f;
+		[field: SerializeField]
+		public float JuggleTime { get; private set; } = 1.25f;
+		[field: SerializeField]
+		public float HandPassHeight { get; private set; } = 0.6f;
+		[field: SerializeField]
+		public float HandPassTime { get; private set; } = 0.6f;
+		[field: SerializeField]
+		public float ThrowTime { get; private set; } = 4f;
+		[field: SerializeField]
+		public float ThrowHeight { get; private set; } = 5f;
+
+		public float TotalJuggleTime => JuggleTime + HandPassTime;
+
+		private List<JuggleBall> balls = new ();
+		private int prevBallsCount;
+		private int delayIndex;
+		private bool isNewJuggle;
+		private bool readyToReceiveBall;
+		
+		public event Action<JugglingBehaviour> OnReadyToReceiveBall;
+		
+		private void Awake()
+		{
+			for (int i = 0; i < startingBallsCount; i++)
+			{
+				if (i > maxBallsCount)
+				{
+					Debug.LogError("Too many ballz bro");
+					return;
+				}
+				
+				var ball = Instantiate(ballPrefab).GetComponent<JuggleBall>();
+				balls.Add(ball);
+				var step = TotalJuggleTime / startingBallsCount;
+
+				ball.Delay = step * i;
+				ball.transform.position = StartingHand.position;
+				ball.SetJugglingBehaviour(this);
+				ball.OnNewJuggle += HandleNewJuggle;
+			}
+
+			prevBallsCount = startingBallsCount;
+			readyToReceiveBall = true;
+		}
+		
+		public static Vector3 GetParabolicPoint(Vector3 start, Vector3 end, float height, float t)
+		{
+			var y = Mathf.Sin(Mathf.PI * t) * height;
+			return Vector3.Lerp(start, end, t) + Vector3.up * y;
+		}
+
+		public async void ThrowBall(JugglingBehaviour target)
+		{
+			var ball = await AwaitForBallInStartingHand();
+			RemoveBall(ball);
+			ball.Throw(target);
+			ball.OnReachedTarget += BallOnOnReachedTarget;
+
+			void BallOnOnReachedTarget(JuggleBall ball)
+			{
+				target.AddBall(ball);
+				ball.OnReachedTarget -= BallOnOnReachedTarget;
+			}
+		}
+		
+		private async void AddBall(JuggleBall ball)
+		{
+			if (balls.Count == maxBallsCount)
+			{
+				Debug.LogError("Cannot add ball! Reached the max");
+				return;
+			}
+
+			readyToReceiveBall = false;
+			prevBallsCount = balls.Count;
+			balls.Add(ball);
+			ball.SetJugglingBehaviour(this);
+			ball.Delay = float.MaxValue;
+			ball.OnNewJuggle += HandleNewJuggle;
+			ball.transform.position = StartingHand.position;
+			// Await until the first ball reaches the starting hand
+			await UniTask.WaitUntil(() => isNewJuggle);
+			ball.Delay = 0;
+		}
+		
+		private void RemoveBall(JuggleBall ball)
+		{
+			if (!balls.Contains(ball))
+			{
+				Debug.LogError("Cannot remove ball! Not in collection");
+				return;
+			}
+
+			prevBallsCount = balls.Count;
+			balls.Remove(ball);
+			ball.OnNewJuggle -= HandleNewJuggle;
+
+			//TODO: readjust visual correctly
+			readyToReceiveBall = false;
+		}
+
+		private void HandleNewJuggle(JuggleBall ball)
+		{
+			if (readyToReceiveBall)
+			{
+				delayIndex = 0;
+				isNewJuggle = false;
+				return;
+			}
+
+			// Wait!
+			isNewJuggle = true;
+			var delay = TotalJuggleTime / balls.Count;
+			var step = TotalJuggleTime / prevBallsCount - TotalJuggleTime / balls.Count;
+
+			ball.Delay = delay - step * delayIndex++;
+			readyToReceiveBall = delayIndex >= balls.Count - 1;
+
+			if (readyToReceiveBall)
+			{
+				OnReadyToReceiveBall?.Invoke(this);
+			}
+		}
+
+		// Absolute garbage
+		private async UniTask<JuggleBall> AwaitForBallInStartingHand()
+		{
+			bool done = false;
+			JuggleBall target = null;
+			foreach (var ball in balls)
+			{
+				ball.OnNewJuggle += BallOnOnNewJuggle;
+			}
+			
+			void BallOnOnNewJuggle(JuggleBall ball)
+			{
+				done = true;
+				target = ball;
+				ball.OnNewJuggle -= BallOnOnNewJuggle;
+			}
+			
+			await UniTask.WaitUntil(() => done);
+			
+			return target;
+		}
+	}
+}
